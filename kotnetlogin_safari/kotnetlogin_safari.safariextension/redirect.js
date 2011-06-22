3acfb7a9aff08b4736552597ff22f1ce170f3ae4
Albert Jorissen <aj0r1553n@gmail.com>
2011-06-22 19:30:44 +0200

    Added initial safari implementation

---
+++ kotnetlogin_safari/kotnetlogin_safari.safariextension/redirect.js
@@ -0,0 +1,29 @@
+// Redirect to login when visiting http://toledo.kuleuven.be
+if(document.location.host == 'toledo.kuleuven.be') {
+	document.location.href = 'https://cygnus.cc.kuleuven.be/webapps/asso-toledo-bb_bb60/nosession/login.jsp?config=a';
+}
+
+// Redirect to login when visiting http(s)://netlogin.kuleuven.be
+if(document.location.host == 'netlogin.kuleuven.be') {
+	// Request institute from settings
+	safari.self.tab.dispatchMessage("getSetting",'institute');
+}
+
+// Redirect when the institute is passed
+function redirect(event){
+		// We're only interested in events that provide the institute
+		if(event.name !== 'institute'){
+			return;
+		}
+		// Extract institute name
+		var institute = event.message;
+		// Redirect
+		document.location.href = 'https://netlogin.kuleuven.be/cgi-bin/wayf.pl?inst=' + institute + '&lang=nl&submit=Ga+verder+%2F+Continue';
+}
+
+// Listen for message replies
+safari.self.addEventListener("message", redirect, false);
+
+
+
+
