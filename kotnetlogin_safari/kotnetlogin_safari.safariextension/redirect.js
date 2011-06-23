f5bf7631fb261e45b2a6e32643c34e19f94f6617
Albert Jorissen <aj0r1553n@gmail.com>
2011-06-23 18:56:20 +0200

    Refactored code

--- kotnetlogin_safari/kotnetlogin_safari.safariextension/redirect.js
+++ kotnetlogin_safari/kotnetlogin_safari.safariextension/redirect.js
@@ -1,3 +1,6 @@
+// Key to send with requests for information
+var REDIRECT_KEY = 'redirect';
+
 // Redirect to login when visiting http://toledo.kuleuven.be
 if(document.location.host == 'toledo.kuleuven.be') {
 	document.location.href = 'https://cygnus.cc.kuleuven.be/webapps/asso-toledo-bb_bb60/nosession/login.jsp?config=a';
@@ -6,17 +9,17 @@ if(document.location.host == 'toledo.kuleuven.be') {
 // Redirect to login when visiting http(s)://netlogin.kuleuven.be
 if(document.location.host == 'netlogin.kuleuven.be') {
 	// Request institute from settings
-	safari.self.tab.dispatchMessage("getSetting",'institute');
+	safari.self.tab.dispatchMessage("getInfo",REDIRECT_KEY);
 }
 
 // Redirect when the institute is passed
 function redirect(event){
-		// We're only interested in events that provide the institute
-		if(event.name !== 'institute'){
+		// We're only interested in events triggered by our request
+		if(event.name !== REDIRECT_KEY){
 			return;
 		}
 		// Extract institute name
-		var institute = event.message;
+		var institute = event.message.institute;
 		// Redirect
 		document.location.href = 'https://netlogin.kuleuven.be/cgi-bin/wayf.pl?inst=' + institute + '&lang=nl&submit=Ga+verder+%2F+Continue';
 }
