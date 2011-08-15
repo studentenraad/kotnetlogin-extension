b518f78b332806d3344a1e4bcc519b2e2cb884eb
Albert Jorissen <aj0r1553n@gmail.com>
2011-08-15 10:41:21 +0200

    Refactored safari and chrome extension to enable reuse of library code True wrong password protection, instead of a counter method.

---
+++ kotnetlogin_safari/kotnetlogin_safari.safariextension/init.js
@@ -0,0 +1,23 @@
+safari.self.addEventListener("message", function(response){
+	if(response.name != "settings"){
+		return;
+	}
+	
+	var settings = response.message;
+	if(settings.username == ''){
+		alert('Please enter your KULeuven Association credentials in the extension. Then refresh this page to have them filled in automatically.');
+		return;
+	}
+	
+	if(!settings.active){
+		return;
+	}
+	
+	try{
+		redirect(document,settings);
+		login(document,settings);
+	} catch(err){
+		alert('De Kotnet Login Extensie kan je niet inloggen. Heb je onlangs je wachtwoord veranderd? Verander dan aub je wachtwoord in de instellingen.');
+	}
+}, false);
+safari.self.tab.dispatchMessage("getSettings",null);
--- kotnetlogin_safari/kotnetlogin_safari.safariextension/redirect.js
+++ kotnetlogin_safari/kotnetlogin_safari.safariextension/redirect.js
@@ -1,30 +1,12 @@
-// Key to send with requests for information
-var REDIRECT_KEY = 'redirect';
-
-// Redirect to login when visiting http://toledo.kuleuven.be
-if(document.location.host == 'toledo.kuleuven.be') {
-	document.location.href = 'https://cygnus.cc.kuleuven.be/webapps/asso-toledo-bb_bb60/nosession/login.jsp?config=a';
-}
-
-// Redirect to login when visiting http(s)://netlogin.kuleuven.be
-if(document.location.href == 'https://netlogin.kuleuven.be/' || document.location.href == 'https://netlogin.kuleuven.be/campusnet/' || document.location.href == 'https://netlogin.kuleuven.be/cgi-bin/netlogout.pl') {
-	// Request institute from settings
-	if(document.location.href.indexOf('wayf.pl') == -1){
-		safari.self.tab.dispatchMessage("getInfo",REDIRECT_KEY);
+function redirect(document,settings){
+ 	// Redirect to login when visiting http://toledo.kuleuven.be
+	if(document.location.host == 'toledo.kuleuven.be') {
+		document.location.href = 'https://cygnus.cc.kuleuven.be/webapps/asso-toledo-bb_bb60/nosession/login.jsp?config=a';
 	}
-}
 
-// Redirect when the institute is passed
-function redirect(event){
-		// We're only interested in events triggered by our request
-		if(event.name !== REDIRECT_KEY){
-			return;
-		}
-		// Extract institute name
-		var institute = event.message.institute;
-		// Redirect
-		document.location.href = 'https://netlogin.kuleuven.be/cgi-bin/wayf.pl?inst=' + institute + '&lang=nl&submit=Ga+verder+%2F+Continue';
+	// Redirect to login when visiting http(s)://netlogin.kuleuven.be
+	if(document.location.href == 'https://netlogin.kuleuven.be/' || document.location.href == 'https://netlogin.kuleuven.be/campusnet/' || document.location.href == 'https://netlogin.kuleuven.be/cgi-bin/netlogout.pl') {
+		// Request institute from settings
+		document.location.href = 'https://netlogin.kuleuven.be/cgi-bin/wayf.pl?inst=' + settings.institute + '&lang=nl&submit=Ga+verder+%2F+Continue';
+	}	
 }
-
-// Listen for message replies
-safari.self.addEventListener("message", redirect, false);
